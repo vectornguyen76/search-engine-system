7299fb6c5d59c658715e0a3c9d0e799ec6480580
Vector Nguyen <vectornguyen76@gmail.com>
2023-08-16 21:21:28 +0700

    ADD comments and docstrings

--- image-search-engine/main.py
+++ image-search-engine/main.py
@@ -4,26 +4,42 @@ from faiss_search import FaissSearch
 from datetime import datetime
 from config import settings
 
-app = FastAPI()
+# Create a FastAPI app instance with the specified title from settings
 app = FastAPI(title=settings.APP_NAME)
 
+# Initialize the feature extractor and FaissSearch instances
 feature_extractor = FeatureExtractor()
 faiss_search = FaissSearch()
 
 @app.post("/search-image")
 async def upload_image(file: UploadFile = File(...)):
+    """
+    Endpoint to upload an image, extract features, and perform a search.
+
+    Parameters:
+        file (UploadFile): The image file to be uploaded.
+
+    Returns:
+        dict: A dictionary containing the uploaded filename.
+    """
+    # Prepend the current datetime to the filename
     file.filename = datetime.now().strftime("%Y%m%d-%H%M%S-") + file.filename
     
+    # Construct the full image path based on the settings
     image_path = settings.IMAGEDIR + file.filename
     
+    # Read the contents of the uploaded file asynchronously
     contents = await file.read()
  
+    # Write the uploaded contents to the specified image path
     with open(image_path, "wb") as f:
         f.write(contents)
         
+    # Extract features from the uploaded image using the feature extractor
     feature = feature_extractor.extract_feature(image_path=image_path)
     
+    # Perform a search using the extracted feature vector
     result = faiss_search.search(query_vector=feature, top_k=3)
     
+    # Return a dictionary with the uploaded filename
     return {"filename": file.filename}
-
