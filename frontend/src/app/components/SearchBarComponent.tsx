3a33c2c48e8fab944f4b0f9e7701b803c6f866ca
Vector Nguyen <vectornguyen76@gmail.com>
2023-08-20 20:43:51 +0700

    ADD frontend

---
+++ frontend/src/app/components/SearchBarComponent.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react'
+
+type Props = {}
+
+const SearchBarComponent = (props: Props) => {
+  return (
+    <div className='m-10'>
+      SearchBarComponent
+    </div>
+  )
+}
+
+export default SearchBarComponent
---
+++ frontend/src/app/libs/useAxios.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const useAxios = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}`
+})
+
+export default useAxios;
