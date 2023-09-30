ccaf07ee44006b278a883b73100cec1b7658dc42
Ubuntu <ubuntu@ip-172-31-24-44.ec2.internal>
2023-09-30 04:40:50 +0000

    Update frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -25,6 +25,19 @@ export default function Home() {
   const NEXT_PUBLIC_IMAGE_SEARCH = process.env.NEXT_PUBLIC_IMAGE_SEARCH!;
   const NEXT_PUBLIC_TEXT_SEARCH = process.env.NEXT_PUBLIC_TEXT_SEARCH!;
 
+  useEffect(() => {
+    // This effect runs when the component mounts
+    const apiUrl = `${NEXT_PUBLIC_TEXT_SEARCH}/full-text-search?query=Ao&size=20`; // You can set your initial API URL here
+    fetch(apiUrl, { method: 'GET' })
+      .then((response) => response.json())
+      .then((data) => {
+        setDataSearch(data)
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []); // The empty dependency array ensures that this effect runs only once when the component mounts
+
   const handleClickSearch = async () => {
     setLoading(true)
     const apiUrl = `${NEXT_PUBLIC_TEXT_SEARCH}/full-text-search?query=${searchValue}&size=20`
@@ -85,10 +98,10 @@ export default function Home() {
         {/* Search bar */}
         <div className='search m-10'>
           <div className="flex items-center justify-center">
-            <div className="flex border-2 rounded">
+            <div className="flex border-2 rounded w-6/12">
               <input
                 type="text"
-                className="px-4 py-2 w-80"
+                className="px-4 py-2 w-full"
                 placeholder="Search..."
                 onChange={(e) => setSearchValue(e.target.value)}
                 onKeyUp={(e) => {
@@ -97,9 +110,10 @@ export default function Home() {
                   }
                 }}
               />
-              <button className="flex items-center justify-center px-4 border-l" value={searchValue} onClick={handleClickUpload}>
+              {/* <button className="flex items-center justify-center px-4 border-l" value={searchValue} onClick={handleClickUpload}>
                 <AiFillCamera />
-              </button>
+              </button> */}
+
               <button className="flex items-center justify-center px-4 border-l" value={searchValue} onClick={handleClickSearch}>
                 <svg className="w-6 h-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24">
@@ -110,35 +124,41 @@ export default function Home() {
             </div>
           </div>
         </div>
+        
+        <div className="flex items-center justify-center w-9/12 mb-12 mx-auto">
+          <label className="flex flex-col items-center justify-center 
+          w-full h-64 border-4 border-gray-300 border-dashed rounded-lg 
+          cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 
+          hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 
+          dark:hover:bg-gray-600">
+            <div className="flex flex-col items-center justify-center pt-5 pb-6">
+              <svg
+                className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 16"
+              >
+                {/* SVG path here */}
+              </svg>
+              <p className="mb-2 text-lg text-gray-500 dark:text-gray-400">
+                <span className="font-semibold">Click to upload</span> or drag and drop
+              </p>
+              <p className="text-lg text-gray-500 dark:text-gray-400">SVG, PNG, JPG, JPEG</p>
+            </div>
+            {/* Hide the default file input */}
+            <input
+              id="dropzone-file"
+              type="file"
+              className="hidden"
+              onChange={handleFileUpload} // Attach the event handler here
+            />
+          </label>
+        </div>
 
-        {showUpload ? (
-          <div className="flex items-center justify-center w-full mb-12">
-            <label className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-              <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                <svg
-                  className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 16"
-                >
-                  {/* SVG path here */}
-                </svg>
-                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                  <span className="font-semibold">Click to upload</span> or drag and drop
-                </p>
-                <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-              </div>
-              <input
-                id="dropzone-file"
-                type="file"
-                className="hidden"
-                onChange={handleFileUpload} // Attach the event handler here
-              />
-            </label>
-          </div>
-        ) : (<> </>)}
-
+          {/* {showUpload ? (
+        ) : (<> </>)} */}
+        <h3 className="text-3xl text-black pb-8 font-medium">Recommendation results</h3>
         {/* Data search */}
         {
           loading ? (
@@ -146,11 +166,15 @@ export default function Home() {
               <p>Loading...</p>
             </>
           ) : (
+            
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
               {dataSearch.map((result, index) => (
                 <div className="relative flex flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md" key={index}>
                   <a className="relative mx-3 mt-3 flex items-center justify-center h-full overflow-hidden rounded-xl" href={result.item_path} target="_blank">
+                    {/* Image item */}
                     <img className="object-cover" src={result.item_image} alt="product image" />
+                    
+                    {/* Sale rate */}
                     {result.sale_rate > 0 && ( // Check if sale_rate is greater than 0
                     <span className="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">
                       {Math.round(result.sale_rate * 100)}% OFF
@@ -158,21 +182,43 @@ export default function Home() {
                     )}
                   </a>
                   <div className="mt-4 px-5 pb-5">
+                    {/* Item name */}
                     <div className="line-clamp-2 text-xl tracking-tight text-slate-900">{result.item_name}</div>
                     <div className="mt-4 flex">
+                      {/* Fix item price */}
                       <p>
                         <span className="text-xl line-through text-black/[0.54] font-medium">₫{result.fixed_item_price.toLocaleString('en-US')}</span>
                       </p>
+                      {/* Salve item price */}
                       <p>
                         <span className="text-xl text-[#ee4d2d] pl-5 font-bold">₫{result.sale_item_price.toLocaleString('en-US')}</span>
                       </p>
                     </div>
                     <br />
+
+                    {/* Sale number */}
                     {result.sales_number > 0 && (
                       <span className="text-xl text-slate-900 font-normal">
                         Đã bán {result.sales_number < 1000 ? result.sales_number : result.sales_number / 1000 + 'k'}
                       </span>
                     )}
+
+                    {/* Rating Star */}
+                    <div className="flex items-center">
+                      {Array.from({ length: 5 }, (_, index) => (
+                        <svg
+                          key={index}
+                          aria-hidden="true"
+                          className="h-5 w-5 text-yellow-300"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                        </svg>
+                      ))}
+                      <span className="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">5.0</span>
+                    </div>
                   </div>
                 </div>
               ))}
@@ -183,21 +229,3 @@ export default function Home() {
     </>
   )
 }
-{/* <div className="flex items-center">
-  <svg aria-hidden="true" className="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-  </svg>
-  <svg aria-hidden="true" className="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-  </svg>
-  <svg aria-hidden="true" className="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-  </svg>
-  <svg aria-hidden="true" className="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-  </svg>
-  <svg aria-hidden="true" className="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-  </svg>
-  <span className="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">5.0</span>
-</div> */}
