83cb0bbd1050641c2b134ede47353a821340e712
Vector Nguyễn <vectonguyen76@gmail.com>
2023-11-02 09:13:13 +0000

    Add login google

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,3 +1,5 @@
+import Appbar from "./components/Appbar";
+import Providers from "./components/Providers";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -17,7 +19,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          <Appbar />
+
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
