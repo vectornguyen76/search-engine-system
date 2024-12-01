8d108095ef2c2ba9659510ca8e297913b415319b
Ubuntu <ubuntu@ip-10-0-1-253.ap-southeast-1.compute.internal>
2024-12-01 14:17:16 +0000

    Update full CICD pipeline

--- image_search/entrypoint.sh
+++ image_search/entrypoint.sh
@@ -3,8 +3,8 @@
 echo "Start ingest data to Faiss search..."
 python faiss_ingest.py
 
-echo "Start ingest data to Qdrant search..."
-python qdrant_ingest.py
+# echo "Start ingest data to Qdrant search..."
+# python qdrant_ingest.py
 
 echo "Run app with uvicorn server..."
 uvicorn app:app --port 7000 --host 0.0.0.0 --workers 1
